168f4eecad1b7222dc2b4eca9bcc144ed49c5a1b
dojoh <ole.johannsen@dkfz.de>
2024-02-27 15:31:48 +0100

    Update dataset.py
    hardcoded different versions of descriptor.json

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import json
 import pathlib
+import copy
 
 import numpy as np
 import torch
@@ -11,7 +12,11 @@ from PIL import Image, ImageDraw
 class DroneImages(torch.utils.data.Dataset):
     def __init__(self, root: str = 'data'):
         self.root = pathlib.Path(root)
-        self.parse_json(self.root / 'descriptor.json')
+        
+        self.parse_json(self.root / 'old_descriptor.json')
+        self.old_ids, self.old_images, self.old_polys, self.old_bboxes = self.ids, self.images, self.polys, self.bboxes
+        self.parse_json(self.root / 'new_descriptor.json')
+        self.new_ids, self.new_images, self.new_polys, self.new_bboxes = self.ids, self.images, self.polys, self.bboxes
 
     def parse_json(self, path: pathlib.Path):
         """
@@ -47,27 +52,44 @@ class DroneImages(torch.utils.data.Dataset):
 
         The corresponding segmentation mask is binary with dimensions [H x W].
         """
-        image_id = self.ids[index]
+        if index <= 322:
+            image_id = self.old_ids[index]
+        else:
+            image_id = self.new_ids[index]
 
         # deserialize the image from disk
-        x = np.load(self.images[image_id])
+        if index <= 322:
+            x = np.load(self.old_images[image_id])
+        else:
+            x = np.load(self.new_images[image_id])
 
-        polys = self.polys[image_id]
-        bboxes = self.bboxes[image_id]
-        masks = []
+        if index <= 322:
+            polys = self.old_polys[image_id]
+            bboxes = self.old_bboxes[image_id]
+            masks = []
+        else:
+            polys = self.new_polys[image_id]
+            bboxes = self.new_bboxes[image_id]
+            masks = []
+            
         # generate the segmentation mask on the fly
         for poly in polys:
             mask = Image.new('L', (x.shape[1], x.shape[0],), color=0)
             draw = ImageDraw.Draw(mask)
-            draw.polygon(poly[0], fill=1, outline=1)
+            if index <= 322:
+                draw.polygon([i + ((ind + 1) % 2) * 60 for ind,i in zip(range(len(poly[0])), poly[0])], fill=1, outline=1)
+            else:
+                draw.polygon(poly[0], fill=1, outline=1)
+                
             masks.append(np.array(mask))
 
         masks = torch.tensor(np.array(masks))
-
         labels = torch.tensor([1 for a in polys], dtype=torch.int64)
 
         boxes = torch.tensor(bboxes, dtype=torch.float)
+        
         # bounding boxes are given as [x, y, w, h] but rcnn expects [x1, y1, x2, y2]
+        boxes[:, 0] += 60 if index <= 322 else 0
         boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
         boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
 
