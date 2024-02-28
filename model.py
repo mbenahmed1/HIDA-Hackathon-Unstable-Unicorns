285d038d1d4fd2deb2fcd9f84c7a046c0308b638
abid-abrar <abid.abrar@helmholtz-munich.de>
2024-02-28 12:03:23 +0100

    added UNET and SwinUNETR

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from torch import nn
 
 from torchvision.models.detection import maskrcnn_resnet50_fpn_v2
 
+from monai.networks.nets import UNet, SwinUNETR
 
 def MaskRCNN(in_channels=5, num_classes=2, image_mean=None, image_std=None, **kwargs):
     if image_mean is None:
@@ -17,3 +18,34 @@ def MaskRCNN(in_channels=5, num_classes=2, image_mean=None, image_std=None, **kw
     model.backbone.body.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False).requires_grad_(True)
 
     return model
+
+def UNet_model(in_channels=5, num_classes=2, **kwargs):
+    
+    model = UNet(
+        spatial_dims=2,
+        in_channels=in_channels,
+        out_channels=num_classes,
+        channels=(4, 8, 16, 32, 64, 256),
+        strides=(2, 2, 2, 2, 2),
+        num_res_units=2,
+        dropout = 0.2
+    )
+    
+    return model
+
+def SwinUNETR_model(in_channels=5, num_classes=2, **kwargs):
+        model = SwinUNETR(
+                    spatial_dims=2,
+                    img_size=(2688, 3392),
+                    in_channels=in_channels,
+                    out_channels=num_classes,
+                    feature_size=48,
+                    depths=[2, 2, 2, 2],
+                    num_heads=[3, 6, 12, 24],
+                    drop_rate=0.2,
+                    attn_drop_rate=0.1,
+                    use_checkpoint=True,
+                    use_v2=True
+                )
+        
+        return model
